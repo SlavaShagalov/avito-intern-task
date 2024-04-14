77052ee71bd8b1beac0ad451345cb6c52b484664
SlavaShagalov <slavikshagalov@gmail.com>
2024-04-14 14:00:56 +0300

    Fix reports.

--- test/stress/test_list.sh
+++ test/stress/test_list.sh
@@ -2,6 +2,6 @@
 
 # Получение всех баннеров (3 штуки).
 
-ab -n 1000 -c 1000 \
+ab -n 1000 -c 1 \
 -H "token: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3MTMxNjU5ODQsImlzX2FkbWluIjp0cnVlLCJ1c2VyX2lkIjoxfQ.DxZ9qQW2ydo6gsH22EqEkkKavVntM2XJpsay9Wa1y5M" \
 "http://localhost:8000/api/v1/banner"
